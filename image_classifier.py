e3b4f913fb1360a13054fdc8b1bf05ea982e1cdf
yunik maharjan <yunik.maharjan@icloud.com>
2018-05-10 19:33:39 +0545

    training added

--- image_classifier.py
+++ image_classifier.py
@@ -19,3 +19,20 @@ X_train /= 255.0
 X_test /= 255.0
 y_test = to_categorical(y_test, 10)
 y_train = to_categorical(y_train, 10)
+
+cnn = Sequential()
+
+cnn.add(Dense(512, activation='relu', input_shape=(784,)))
+
+cnn.add(Dense(512, activation='relu'))
+cnn.add(Dense(10, activation='softmax'))
+cnn.compile(optimizer='adam', loss="categorical_crossentropy",
+            metrics=['accuracy'])
+cnn.summary()
+history = cnn.fit(X_train, y_train, epochs=5, validation_data=(X_test, y_test))
+
+plt.plot(history.history['acc'], 'g-')
+plt.plot(history.history['val_acc'], 'b-')
+
+plt.plot(history.history['loss'], 'r-')
+plt.xlim(0, 4)
