a2f10e9674d34b5a1905100a91d0d92ced797894
yunik maharjan <yunik.maharjan@icloud.com>
2018-05-10 20:19:52 +0545

    minor change

--- image_classifier.py
+++ image_classifier.py
@@ -7,46 +7,49 @@ import numpy as np
 import cv2
 import sys
 import os
+import matplotlib.pyplot as plt
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
-# import matplotlib.pyplot as plt
-# (X_train, y_train), (X_test, y_test) = mnist.load_data()
-# num_classes = 10
-# epochs = 3
 
-# X_train = X_train.reshape(60000, 28*28)
-# X_test = X_test.reshape(10000, 28*28)
+if not sys.argv.__len__() >= 2:
+    (X_train, y_train), (X_test, y_test) = mnist.load_data()
+    num_classes = 10
+    epochs = 5
 
-# X_train = X_train.astype('float32')
-# X_test = X_test.astype('float32')
+    X_train = X_train.reshape(60000, 28*28)
+    X_test = X_test.reshape(10000, 28*28)
 
-# X_train /= 255.0
-# X_test /= 255.0
-# y_test = to_categorical(y_test, 10)
-# y_train = to_categorical(y_train, 10)
+    X_train = X_train.astype('float32')
+    X_test = X_test.astype('float32')
 
-# model = Sequential()
+    X_train /= 255.0
+    X_test /= 255.0
+    y_test = to_categorical(y_test, num_classes)
+    y_train = to_categorical(y_train, num_classes)
 
-# model.add(Dense(512, activation='relu', input_shape=(784,)))
+    model = Sequential()
 
-# model.add(Dense(512, activation='relu'))
-# model.add(Dense(10, activation='softmax'))
-# model.compile(optimizer='adam', loss="categorical_crossentropy",
-#               metrics=['accuracy'])
-# model.summary()
-# history = model.fit(X_train, y_train, epochs=5,
-#                     validation_data=(X_test, y_test))
+    model.add(Dense(512, activation='relu', input_shape=(784,)))
 
-# plt.plot(history.history['acc'], 'g-')
-# plt.plot(history.history['val_acc'], 'b-')
+    model.add(Dense(512, activation='relu'))
+    model.add(Dense(10, activation='softmax'))
+    model.compile(optimizer='adam', loss="categorical_crossentropy",
+                  metrics=['accuracy'])
+    model.summary()
+    history = model.fit(X_train, y_train, epochs=epochs,
+                        validation_data=(X_test, y_test))
 
-# plt.plot(history.history['loss'], 'r-')
-# plt.xlim(0, 4)
-model = load_model('classifier.h5')
+    plt.plot(history.history['acc'], 'g-')
+    plt.plot(history.history['val_acc'], 'b-')
 
-tr = cv2.imread(sys.argv[1], 0)
-tr = cv2.resize(tr, (28, 28))
-tr = tr.astype('float32')
-t = tr/255
-t = t.flatten()
-t = np.array([t])
-print(model.predict(t).argmax())
+    plt.plot(history.history['loss'], 'r-')
+    plt.xlim(0, 4)
+else:
+    model = load_model('classifier.h5')
+
+    tr = cv2.imread(sys.argv[1], 0)
+    tr = cv2.resize(tr, (28, 28))
+    tr = tr.astype('float32')
+    t = tr/255
+    t = t.flatten()
+    t = np.array([t])
+    print(model.predict(t).argmax())
